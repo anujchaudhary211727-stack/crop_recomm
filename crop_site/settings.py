1fb54aa636d057c862d549928a2034f815c00d60
Anuj Chaudhary <anujchaudhary211727@gmail.com>
2026-07-15 11:59:33 +0530

    Add deployment configuration for Render

--- crop_site/settings.py
+++ crop_site/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ykl!%dp0g-n(j(hg$2t26geffnc$2%s0d-mub+#pzj^q!ph3)8'
+import os
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-ykl!%dp0g-n(j(hg$2t26geffnc$2%s0d-mub+#pzj^q!ph3)8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -115,7 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
